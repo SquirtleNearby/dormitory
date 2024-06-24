b2097c6a10e0e5896d59efd30be35b9f450ae44f
SquirtleNearby <aziankev23101999@gmail.com>
2024-06-24 21:22:39 +0500

    Добавлены комменты к js, перемещены стили, адаптирован главный макет

--- js/request.js
+++ js/request.js
@@ -1,3 +1,4 @@
+// Отображать заявки при клике на стрелочку и переворачивать её
 document.querySelectorAll('.dropDownButton').forEach(button => {
   button.addEventListener('click', () => {
     const contentDiv = button.closest('.main-additional-info-content__request__closed').querySelector('.main-additional-info-content__request__open');
@@ -10,18 +11,18 @@ document.querySelectorAll('.dropDownButton').forEach(button => {
   });
 });
 
-
+// Скрывать/открывать иконки изменения объявлений при клике на заголовок
 let hidden = false;
 const styleElement = document.createElement('style');
 styleElement.innerHTML = `
             .main-additional-info-content_title::after,
             .main-additional-info-content_title::before,
             .main-additional__info::after,
-            .main-additional_info__openPopup {
+            .main-additional_info__openPopup,
+            .main-additional-info-content__delete {
                 display: inline !important;
             }
         `;
-
 document.querySelector('.main-additional__info__title').addEventListener('click', function () {
   if (hidden) {
     document.head.removeChild(styleElement);
@@ -31,14 +32,44 @@ document.querySelector('.main-additional__info__title').addEventListener('click'
   hidden = !hidden;
 });
 
+// Закрыть ПопАпы
 const closedPopup = document.createElement('style');
 closedPopup.innerHTML = '.main-popup, .main-overlay {display: none !important;}';
-document.querySelector('.main-popup-form_closed').addEventListener('click', function () {
+function closePopup() {
   document.head.appendChild(closedPopup);
-})
+}
+document.querySelector('.main-popup-form_closed').addEventListener('click', closePopup);
+document.querySelector('.main-overlay').addEventListener('click', closePopup);
 
+// Открыть ПопАпы
 const openPopup = document.createElement('style');
 openPopup.innerHTML = '.main-popup, .main-overlay {display: flex !important}';
 document.querySelector('.main-additional_info__openPopup').addEventListener('click', function () {
   document.head.appendChild(openPopup);
 })
+
+// Проверка на наличие сообщений
+const contentContainer = document.querySelector('.main-additional-info-content');
+function updateNoMessages() {
+  if (document.querySelectorAll('.main-additional-info-content_message').length === 0) {
+    const noMessagesElement = document.createElement('h3');
+    noMessagesElement.classList.add('main-additional_info__noMessages');
+    noMessagesElement.textContent = 'На данный момент объявления отсутствуют';
+    contentContainer.appendChild(noMessagesElement);
+  } else {
+    const noMessagesElement = document.querySelector('.no-messages');
+    if (noMessagesElement) {
+      noMessagesElement.remove();
+    }
+  }
+}
+
+// Удалить ближайшее к кнопке удаления объявление
+document.querySelectorAll('.main-additional-info-content__delete').forEach(button => {
+  button.addEventListener('click', () => {
+    const oneMessage = button.closest('.main-additional-info-content_message');
+    oneMessage.remove();
+    updateNoMessages();
+  })
+})
+
