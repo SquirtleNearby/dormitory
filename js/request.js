825efd3683823513169429f20c791154f880668e
SquirtleNearby <aziankev23101999@gmail.com>
2024-06-25 12:59:36 +0500

    Создана страница заявок, изменены иконки на прачечной, изменена главная страница

--- js/request.js
+++ js/request.js
@@ -1,16 +1,3 @@
-// Отображать заявки при клике на стрелочку и переворачивать её
-document.querySelectorAll('.dropDownButton').forEach(button => {
-  button.addEventListener('click', () => {
-    const contentDiv = button.closest('.main-additional-info-content__request__closed').querySelector('.main-additional-info-content__request__open');
-    if (contentDiv.style.display === 'none' || contentDiv.style.display === '') {
-      contentDiv.style.display = 'block';
-    } else {
-      contentDiv.style.display = 'none';
-    }
-    button.classList.toggle('open');
-  });
-});
-
 // Скрывать/открывать иконки изменения объявлений при клике на заголовок
 let hidden = false;
 const styleElement = document.createElement('style');
